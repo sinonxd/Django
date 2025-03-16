4deee1a494984b49a1f39f1c2d49b12fe7fed9ec
sinonxd <105306546+sinonxd@users.noreply.github.com>
2025-03-16 15:05:33 -0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,14 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import get_artist_info, get_song_info, UserView, UserDetailView, UserPreferencesView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('spotifyapp.urls')),  # Esto incluye las rutas de spotifyapp
+    # Rutas para la API de Spotify
+    path('spotify/artist/<str:artist_name>/', get_artist_info, name='get_artist_info'),
+    path('spotify/song/<str:song_name>/', get_song_info, name='get_song_info'),
+
+    # Rutas para los usuarios
+    path('users/', UserView.as_view(), name='user_view'),  # Crear usuario y obtener todos los usuarios
+    path('users/<str:user_id>/', UserDetailView.as_view(), name='user_detail_view'),  # Actualizar y eliminar usuario
+    path('users/<str:user_id>/preferences/', UserPreferencesView.as_view(), name='user_preferences_view'),  # Agregar y obtener preferencias
 ]
+
